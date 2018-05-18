479e45ce36a68e69285ebb8afaa0109ffe7fe8be
Mahdi Abbasi <ma.abbasi.9675@gmail.com>
2018-05-18 17:25:57 +0430

    stochastic done

--- network.py
+++ network.py
@@ -15,27 +15,27 @@ def d_sigmoid(x):
     return x*(1-x)
 
 def get_sample_data2(folder_name):
-    X = np.zeros((100, 784))
-    Y = np.zeros((100, 10))
+    X = np.zeros((200, 784))
+    Y = np.zeros((200, 10))
     path = os.getcwd() + '/' + folder_name
     for idx, folder_name in enumerate(os.listdir(path)):
         folder_path = path + '/' + folder_name
         for index, image_name in enumerate(os.listdir(folder_path)):
-            if index == 330:
+            if index == 1320:
                 break
-            if index >= 320:   
+            if index >= 1300:   
                 image_path = folder_path + '/' + image_name
                 image = Image.open(image_path)
                 image = image.convert('1')
                 data = [0 for i in range(10)]
                 data[alphabet_to_int[folder_name]] = 1
-                Y[idx * 10 + index-320] = data
+                Y[idx * 10 + index-1300] = data
                 pixels = image.load()
                 data = []
                 for i in range(28):
                     for j in range(28):
                         data.append(0 if pixels[i,j] == 0 else 1)
-                X[idx * 10 + index-320] = data
+                X[idx * 10 + index-1300] = data
     return X, Y
 
 def get_sample_data(folder_name):
@@ -66,9 +66,50 @@ def calculate_loss(o, y):
 
 def accuracy(o, y):
     s = 0
-    for i in range(len(o)):
-        s += 0.01 if np.argmax(o[i]) == np.argmax(y[i]) else 0
+    n = len(o)
+    t = 1 / float(n)
+    for i in range(n):
+        s += t if np.argmax(o[i]) == np.argmax(y[i]) else 0
     return s
+
+def stochastic_gradient_descent(input_data, result, w_hidden, b_hidden, w_output, b_output, do_dropout, h, r, dropout_percent, lr):
+    for index, data in enumerate(input_data):
+        data2 = np.zeros((1, 784))
+        data2[0] = data
+        result2 = np.zeros((1, 10))
+        result2[0] = result[index]
+        # forward propagation
+        hidden_result = sigmoid(data2.dot(w_hidden) + b_hidden)
+        output_result = hidden_result.dot(w_output) + b_output
+
+        # backward propagation
+        d_output = result2 - output_result
+        d_hidden = d_output.dot(w_output.T) * d_sigmoid(hidden_result)
+        # dropout regularization
+        if(do_dropout):
+            d_hidden *= np.random.binomial([np.ones((len(data2),h))],1-dropout_percent)[0] * (1.0/(1-dropout_percent))
+        w_output += (hidden_result.T.dot(d_output) + w_output * r) * lr
+        w_hidden += (data2.T.dot(d_hidden) + w_hidden * r) * lr
+        b_output += d_output.sum(axis=0) * lr
+        b_hidden += d_hidden.sum(axis=0) * lr
+    print calculate_loss(output_result, result)
+
+def gradient_descent(input_data, result, w_hidden, b_hidden, w_output, b_output, do_dropout, h, r, dropout_percent, lr):
+     # forward propagation
+    hidden_result = sigmoid(input_data.dot(w_hidden) + b_hidden)
+    output_result = hidden_result.dot(w_output) + b_output
+
+    # backward propagation
+    d_output = result - output_result
+    d_hidden = d_output.dot(w_output.T) * d_sigmoid(hidden_result)
+    # dropout regularization
+    if(do_dropout):
+        d_hidden *= np.random.binomial([np.ones((len(input_data),h))],1-dropout_percent)[0] * (1.0/(1-dropout_percent))
+    w_output += (hidden_result.T.dot(d_output) + w_output * r) * lr
+    w_hidden += (input_data.T.dot(d_hidden) + w_hidden * r) * lr
+    b_output += d_output.sum(axis=0) * lr
+    b_hidden += d_hidden.sum(axis=0) * lr
+    print calculate_loss(output_result, result)
 
 def main():
 
@@ -89,22 +130,9 @@ def main():
 
     # training NN
     for i in range(n):
-        # forward propagation
-        hidden_result = sigmoid(input_data.dot(w_hidden) + b_hidden)
-        output_result = hidden_result.dot(w_output) + b_output
-
-        # backward propagation
-        d_output = result - output_result
-        d_hidden = d_output.dot(w_output.T) * d_sigmoid(hidden_result)
-        # dropout regularization
-        if(do_dropout):
-            d_hidden *= np.random.binomial([np.ones((len(input_data),h))],1-dropout_percent)[0] * (1.0/(1-dropout_percent))
-        w_output += (hidden_result.T.dot(d_output) + w_output * r) * lr
-        w_hidden += (input_data.T.dot(d_hidden) + w_hidden * r) * lr
-        b_output += d_output.sum(axis=0) * lr
-        b_hidden += d_hidden.sum(axis=0) * lr
-        if i%100 == 0:
-            print calculate_loss(output_result, result)
+        # stochastic_gradient_descent(input_data, result, w_hidden, b_hidden, w_output, b_output, do_dropout, h, r, dropout_percent, lr)
+        gradient_descent(input_data, result, w_hidden, b_hidden, w_output, b_output, do_dropout, h, r, dropout_percent, lr)
+        print "iteration: " + str(i + 1)
     
     # testing NN
     X, Y = get_sample_data2('notMNIST_small')
