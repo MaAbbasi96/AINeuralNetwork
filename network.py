e83ebfda0a85bce7e4c9a2859bbc0042dca7df05
Mahdi Abbasi <ma.abbasi.9675@gmail.com>
2018-05-18 18:29:44 +0430

    done plot

--- network.py
+++ network.py
@@ -3,6 +3,7 @@ from PIL import Image
 from math import exp
 import random
 import numpy as np
+from matplotlib import pyplot
 
 alphabet_to_int = {
     'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7, 'I': 8, 'J': 9
@@ -21,21 +22,21 @@ def get_sample_data2(folder_name):
     for idx, folder_name in enumerate(os.listdir(path)):
         folder_path = path + '/' + folder_name
         for index, image_name in enumerate(os.listdir(folder_path)):
-            if index == 1320:
+            if index == 1220:
                 break
-            if index >= 1300:   
+            if index >= 1200:   
                 image_path = folder_path + '/' + image_name
                 image = Image.open(image_path)
                 image = image.convert('1')
                 data = [0 for i in range(10)]
                 data[alphabet_to_int[folder_name]] = 1
-                Y[idx * 10 + index-1300] = data
+                Y[idx * 10 + index-1200] = data
                 pixels = image.load()
                 data = []
                 for i in range(28):
                     for j in range(28):
                         data.append(0 if pixels[i,j] == 0 else 1)
-                X[idx * 10 + index-1300] = data
+                X[idx * 10 + index-1200] = data
     return X, Y
 
 def get_sample_data(folder_name):
@@ -72,7 +73,7 @@ def accuracy(o, y):
         s += t if np.argmax(o[i]) == np.argmax(y[i]) else 0
     return s
 
-def stochastic_gradient_descent(input_data, result, w_hidden, b_hidden, w_output, b_output, do_dropout, h, r, dropout_percent, lr):
+def stochastic_gradient_descent(i, input_data, result, w_hidden, b_hidden, w_output, b_output, do_dropout, h, r, dropout_percent, lr):
     for index, data in enumerate(input_data):
         data2 = np.zeros((1, 784))
         data2[0] = data
@@ -94,7 +95,7 @@ def stochastic_gradient_descent(input_data, result, w_hidden, b_hidden, w_output
         b_hidden += d_hidden.sum(axis=0) * lr
     print calculate_loss(output_result, result)
 
-def gradient_descent(input_data, result, w_hidden, b_hidden, w_output, b_output, do_dropout, h, r, dropout_percent, lr):
+def gradient_descent(l, i, input_data, result, w_hidden, b_hidden, w_output, b_output, do_dropout, h, r, dropout_percent, lr):
      # forward propagation
     hidden_result = sigmoid(input_data.dot(w_hidden) + b_hidden)
     output_result = hidden_result.dot(w_output) + b_output
@@ -109,7 +110,11 @@ def gradient_descent(input_data, result, w_hidden, b_hidden, w_output, b_output,
     w_hidden += (input_data.T.dot(d_hidden) + w_hidden * r) * lr
     b_output += d_output.sum(axis=0) * lr
     b_hidden += d_hidden.sum(axis=0) * lr
-    print calculate_loss(output_result, result)
+    loss = calculate_loss(output_result, result)
+    print loss
+    if i % 100 == 0:
+        l.append((i, loss))
+    return l
 
 def main():
 
@@ -124,15 +129,24 @@ def main():
     b_hidden = np.zeros(h)
     w_output = np.random.uniform(-0.01, 0.01, (h, 10))
     b_output = np.zeros(10)
+    l = []
 
     # getting input
     input_data, result = get_sample_data('notMNIST_small')
 
     # training NN
     for i in range(n):
-        # stochastic_gradient_descent(input_data, result, w_hidden, b_hidden, w_output, b_output, do_dropout, h, r, dropout_percent, lr)
-        gradient_descent(input_data, result, w_hidden, b_hidden, w_output, b_output, do_dropout, h, r, dropout_percent, lr)
+        # stochastic_gradient_descent(i, input_data, result, w_hidden, b_hidden, w_output, b_output, do_dropout, h, r, dropout_percent, lr)
+        gradient_descent(l, i, input_data, result, w_hidden, b_hidden, w_output, b_output, do_dropout, h, r, dropout_percent, lr)
         print "iteration: " + str(i + 1)
+    lx = []
+    ly = []
+    for i in l:
+        lx.append(i[0])
+        ly.append(i[1])
+    print lx, ly
+    pyplot.plot(lx, ly)
+    pyplot.show()
     
     # testing NN
     X, Y = get_sample_data2('notMNIST_small')
